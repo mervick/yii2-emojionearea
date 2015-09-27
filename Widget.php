f539dc57048b5ae8b0cae9f15b9dc4e24f8895aa
Andrey Izman <izman.dev@gmail.com>
2015-09-27 12:19:59 +0300

    Update Widget.php

---
+++ Widget.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace mervick\emojionearea;
+
+use Yii;
+use yii\helpers\Json;
+use yii\widgets\InputWidget;
+use yii\base\InvalidConfigException;
+use yii\helpers\Html;
+use yii\helpers\Url;
+
+/**
+ * Class Widget
+ * @package mervick\emojionearea
+ */
+class Widget extends InputWidget
+{
+
+    /**
+     * @var array the HTML attributes for the textarea input
+     */
+    public $options;
+
+    /**
+     * @var array Plugin options that will be passed to the emojioneArea
+     */
+    public $pluginOptions;
+
+
+    /**
+     * Initialize the widget
+     */
+    public function init()
+    {
+        parent::init();
+
+        $this->generateId();
+        $this->registerAssets();
+        echo $this->renderInput();
+    }
+
+    /**
+     * Generate HTML identifiers for elements
+     */
+    protected function generateId()
+    {
+        if (empty($this->options['id'])) {
+            $this->options['id'] = $this->getId();
+        } else {
+            $this->options['id'] = preg_replace('/[^a-z0-9_\]/i', '_', $this->options['id']);
+        }
+    }
+
+    /**
+     * Register client assets
+     */
+    protected function registerAssets()
+    {
+        $view = $this->getView();
+        AssetBundle::register($view);
+
+        $pluginOptions = !empty($this->pluginOptions) ? Json::encode($this->pluginOptions) : '';
+
+        $js = <<<JS
+    window.emojioneArea_{$this->options['id']} = jQuery("#{$this->options['id']}").emojioneArea($pluginOptions);
+JS;
+        Yii::$app->getView()->registerJs($js);
+    }
+
+    /**
+     * Render the text area input
+     */
+    protected function renderInput()
+    {
+        if ($this->hasModel()) {
+            $input = Html::activeTextArea($this->model, $this->attribute, $this->options);
+        } else {
+            $input = Html::textArea($this->name, $this->value, $this->options);
+        }
+        return $input;
+    }
+}
